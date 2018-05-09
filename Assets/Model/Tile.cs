d2f69276717ae0a043767d83d4ee9a48adb0d8cc
Naoskev <vno4@laposte.net>
2018-05-09 18:22:25 +0200

    Mise en place initiale du projet

---
+++ Assets/Model/Tile.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile {
+
+	int x;
+    int y;
+
+    enum TileType    
+    {
+        Empty, Floor
+    }
+
+    TileType type = TileType.Floor;
+
+    public Tile(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+}

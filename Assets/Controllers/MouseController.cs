b57b35a979de75c94674847a97006ff40d716c5d
Naoskev <vno4@laposte.net>
2018-05-10 23:34:54 +0200

    Ajout d'une fonction de glisser déposer pour modifier les tiles

--- Assets/Controllers/MouseController.cs
+++ Assets/Controllers/MouseController.cs
@@ -7,6 +7,8 @@ public class MouseController : MonoBehaviour {
 	public GameObject circleCursor;
 	private Vector3 lastFrameMousePosition;
 
+	private Vector3? dragMouseStartPosition;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -14,20 +16,54 @@ public class MouseController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 currMouseCoordinate = Camera.main.ScreenToWorldPoint( Input.mousePosition);
-		currMouseCoordinate.z = 0;
+		Vector3 currFramePosition = Camera.main.ScreenToWorldPoint( Input.mousePosition);
+		currFramePosition.z = 0;
 
-		Tile tile = this.getTileFromVector(currMouseCoordinate);
-		if(tile != null){
+		Tile currentTile = this.getTileFromVector(currFramePosition);
+		if(currentTile != null){
 			circleCursor.SetActive(true);
-			circleCursor.transform.position = new Vector3Int(tile.X, tile.Y, 0);
+			circleCursor.transform.position = new Vector3Int(currentTile.X, currentTile.Y, 0);
 		}
 		else {
 			circleCursor.SetActive(false);
 		}
+
+		if(Input.GetMouseButtonDown(0)){
+			dragMouseStartPosition = currFramePosition;
+		}
+		if(Input.GetMouseButtonUp(0) && dragMouseStartPosition.HasValue){
+			int start_x = Mathf.FloorToInt( dragMouseStartPosition.Value.x);
+			int end_x = Mathf.FloorToInt(currFramePosition.x);
+			if(start_x > end_x){
+				int temp = start_x;
+				start_x = end_x;
+				end_x = temp;
+			}
+			int start_y = Mathf.FloorToInt( dragMouseStartPosition.Value.y);
+			int end_y = Mathf.FloorToInt(currFramePosition.y);
+			if(start_y > end_y){
+				int temp = start_y;
+				start_y = end_y;
+				end_y = temp;
+			}
+
+			for (int x = start_x; x <= end_x; x++)
+			{
+				for (int y = start_y; y <= end_y; y++)
+				{
+					Tile tileToChange = WorldController.Instance.World.getTileAt(x, y);
+					if(tileToChange != null){
+						tileToChange.Type = Tile.TileType.Floor;
+					}
+				}
+				
+			}
+
+			dragMouseStartPosition = null;
+		}
 		
 		if(Input.GetMouseButton(1) || Input.GetMouseButton(2)){
-			Vector3 diff = lastFrameMousePosition - currMouseCoordinate;
+			Vector3 diff = lastFrameMousePosition - currFramePosition;
 			Camera.main.transform.Translate(diff);
 		}
 

8b9acf47b4caaa1931ce431c7e096c84016e4b66
Naoskev <vno4@laposte.net>
2018-05-11 15:48:32 +0200

    Ajout des boutons + ajout d'une classe utilitaire modifiant l'éditeur Unity

---
+++ Assets/UI/AutomaticVerticalSize.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutomaticVerticalSize : MonoBehaviour {
+
+
+	public float childsHeight = 35f;
+
+	// Use this for initialization
+	void Start () {
+		this.AdjustSize();
+	}
+
+	public void AdjustSize(){
+		Vector2 size = this.GetComponent<RectTransform>().sizeDelta;
+		size.y = this.transform.childCount * this.childsHeight;
+		this.GetComponent<RectTransform>().sizeDelta = size;
+	}
+
+}
---
+++ Assets/UI/Editor/AutomaticVerticalSizeEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(AutomaticVerticalSize))]
+public class AutomaticVerticalSizeEditor : Editor {
+
+	public override void OnInspectorGUI(){
+		DrawDefaultInspector();
+
+		if(GUILayout.Button("Adjust size")){
+			((AutomaticVerticalSize)this.target).AdjustSize();
+		}
+	}
+}

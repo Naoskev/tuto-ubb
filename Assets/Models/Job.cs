0c7ccfbbab2527d07e34311d797eb38758392846
Naoskev <vno4@laposte.net>
2018-05-30 23:42:22 +0200

    Ajout des job (planification des taches)

---
+++ Assets/Models/Job.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Job {
+
+	Tile tile;
+
+	Action<Job> cbJobComplete;
+
+	Action<Job> cbJobCancelled;
+
+	float jobTime;
+
+
+	public Job(Tile tile, Action<Job> completeCallback,  float jobDuration = 1f){
+		this.tile = tile;
+		this.cbJobComplete = completeCallback;
+		this.jobTime = jobDuration;
+	}
+
+	public void RegisterCompleteCallback(Action<Job> callback){
+		this.cbJobComplete += callback;
+	}
+	public void RegisterCancelledCallback(Action<Job> callback){
+		this.cbJobCancelled += callback;
+	}
+
+	public void DoWork(float workTime){
+		this.jobTime -= workTime;
+
+		if(jobTime <= 0){
+			if(this.cbJobComplete != null){
+				this.cbJobComplete(this);
+			}
+		}
+	}
+
+	public void CancelJob(){
+		if(this.cbJobCancelled != null){
+			this.cbJobCancelled(this);
+		}
+	}
+
+}
